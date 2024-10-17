d3b5b8ba032b30d5171dcb994c48488c2ae12f16
Nix-JPG <nixiez7104@gmail.com>
2024-10-17 08:11:09 -0700

    index.js
    up1

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>WELLCOME</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Selamat Datang Pelayanan Praktikum" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Silahkan <code>LOGIN</code> terlebih dahulu
         </p>
       </main>
 
