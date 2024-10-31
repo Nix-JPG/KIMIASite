ade5a0c80f0e0fc9e491bbfd5adfa538d43c1ad1
Nix-JPG <nixiez7104@gmail.com>
2024-10-31 20:34:02 +0800

    Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="https://ypmic.or.id/logo-uin-png/" alt="UIN Logo" className={styles.logo} />
+        <img src="https://ypmic.or.id/wp-content/uploads/2020/09/logo-uin-png.png" alt="UIN Logo" className={styles.logo} />
       </footer>
     </>
   )
